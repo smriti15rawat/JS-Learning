fb6d9ac3b52614422ea94fc7f3bd4c32a7e1b446
Smriti <39672384+smriti15rawat@users.noreply.github.com>
2023-01-04 09:01:46 +0000

    datatypes

---
+++ 01_Basics/02_DataTypes.js
@@ -0,0 +1,27 @@
+"use strict"; //Treat all code as the new version or new JS.
+
+//alert(3+3) //we are using nodejs , not browser, so this will trough error
+console.log(3+3)
+
+console.log("hi")
+
+let name ="hitesh"
+let age = 23
+let isLoggedIn = false
+
+//DataTypes
+//number => 2 to the power 53
+//bigint
+//string =>""
+//boolean = true, false
+//null=> standalone value
+//undefined => variable is assigned but value is not given, null means it is empty.
+//symbol=> unique
+
+
+//Object
+
+console.log(typeof"")
+console.log(typeof(age))
+console.log(typeof null) // Op- Object
+console.log(typeof undefined) // op- Undefined
