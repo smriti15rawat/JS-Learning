54f0c5a2c47bf91690593b294a4e948ee001be33
Smriti <39672384+smriti15rawat@users.noreply.github.com>
2023-01-05 08:02:28 +0000

    Conversion of datatype

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,26 @@
+let score = null
+
+console.log(typeof score)
+
+let vauleInNumber = Number(score)
+console.log(typeof vauleInNumber)
+console.log(vauleInNumber)
+ //"33" => Number
+ //"33ABC" =>NaN
+ //True => 1, false=>0
+ //undefined  => NaN
+ //null => 0
+
+ let isLoggedIn = "smr"
+ let BooleanLoogedIn = Boolean(isLoggedIn)
+ console.log(BooleanLoogedIn)
+
+ //1=> true , 0=> false
+ // emoty strinh => false
+ //string => true
+
+
+ let someNumber = 33
+ let stringNumber = String(someNumber)
+ console.log(stringNumber)
+ console.log(typeof stringNumber)
